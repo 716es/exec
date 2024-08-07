d2893282990f6a499e22e42e3a90481e4a184873
taka <admin@716.es>
2024-08-07 12:57:05 +0900

    support cross domain usag

--- exec.js
+++ exec.js
@@ -1,8 +1,22 @@
 
+var hashcucond = window.hashcucond || '\\+.';
+var log = function(){
+    if(localStorage.debug) console.log.apply(console, arguments);
+}
+
+if(!window.changeLocationHash){
+    window.changeLocationHash = function(href){
+        log('changeLocationHash to', href);
+        location.href = href;
+    }
+}
+
 function urlcleanup(isEvent){
-    if(location.hash && (location.hash && location.hash.match('\\+')) && !sessionStorage.hashstart){
+    if(location.hash && (location.hash && location.hash.match(hashcucond)) &&
+       !sessionStorage.hashstart){
         if(isEvent){
-            var pn = location.pathname + location.search + location.hash.replace(/\+.*$/g, '');
+            var pn = location.pathname +
+                location.search + location.hash.replace(/(\+).*$/g, '$1');
             if(pn.substr(-1) == '#') pn = pn.replace(/.$/, '');
             history.replaceState({id: pn}, '', pn);
         }else{
@@ -13,110 +27,162 @@ function urlcleanup(isEvent){
     }
     return true;
 }
+
 function sameurl(a, b){
     a = a.replace(/#.+/, '');
     b = b.replace(/#.+/, '');
-    if(a == b) return true;
+    if(a == b) {return true;}
     var ax = document.createElement('a'); ax.href = a;
     var bx = document.createElement('a'); bx.href = b;
-    if(ax.href == bx.href) return true;
+    if(ax.href == bx.href) {return true;}
     return false;
 }
-function exec(href, oldwindow){
-    var hash = href.match('(#.+)$') && RegExp.$1, o = null;
-    oldwindow = !oldwindow && window.opener && opener.location &&
-        sameurl(opener.location.href, href) ? opener : null;
-    if(oldwindow){
-        if(!oldwindow.opened) oldwindow.opened = {};
-        o = oldwindow.opened;
-    }else{
-        if(!window.opened) window.opened = {};
-        o = window.opened;
-    }
-    // console.log('exec:href', href, 'and set opened[hash]', hash, oldwindow);
-    o[hash] = {
-        window: oldwindow ? ((oldwindow.location.href = href) && window) : open(href),
-        status: 0
+
+function exec(href, foundopener){
+    var hash = href.match('(#.+)$') && RegExp.$1, o = null, stat = null;
+    foundopener = !foundopener && parentPort &&
+        sameurl(parentPort.href, href) ? opener : null;
+    log('exec:href', href, 'and set opened[hash]', hash, foundopener);
+    stat = {
+        hash: hash, href: href, childWindow: null,
+        status: 0, port: null
     };
     if(!window.name) window.name = 'exec-source-' + Math.floor(Math.random() * 100000);
+    if(foundopener){
+        log('foundopener', stat, ';', href);
+        // send stat to parentPort and kick hashchange
+        parentPort.postMessage(stat);
+        parentPort.postMessage(href);
+    }else{
+        stat.childWindow = open(href.replace(/(#.+)$/g, ''));
+        if(!window.opened) window.opened = {};
+        o = window.opened;
+        o[hash] = stat;
+        window.addEventListener('message', function portInit(e){
+            if(e.ports && e.ports[0]){
+                o[hash].port = e.ports[0];
+                o[hash].port.onmessage = function(e2){
+                    log('exec, onmessage from childWindow', e2.data);
+                    if(typeof(e2.data) == 'string') {
+                        changeLocationHash(e2.data)
+                    }else if(e2.data.href){
+                        o[e2.data.hash] = e2.data;
+                        o[e2.data.hash].childWindow = stat.childWindow;
+                        o[e2.data.hash].port = this;
+                    }else if(e2.data.hash && e2.data.type == 'complete'){
+                        var e = new CustomEvent('execcomplete');
+                        e.hash = e2.data.hash;
+                        e.result = e2.data.result;
+                        window.dispatchEvent(e);
+                    }
+                };
+                o[hash].port.postMessage(location.href);
+                log('exec: send hash', hash);
+                o[hash].port.postMessage(hash);
+                window.removeEventListener('message', portInit);
+            }
+        });
+    }
     return new Promise(function(r, j){
-        window.addEventListener('hashchange', function execohc(){
-            var stat = o[hash];
-            // console.log('exec: hashchange', stat, ';', location.hash);
-            if(location.hash[1] == '+' && o[hash].status !== 0){
-                r(decodeURIComponent(location.hash.substr(2)));
-                if(!oldwindow){
-                    stat.window.close();
-                }else{
+        window.addEventListener('execcomplete', function execohc(evt){
+            if(evt.hash == hash){
+                var result = decodeURIComponent(evt.result.substr(2));
+                log('execcomplete:', result);
+                r(result);
+                if(foundopener){ // reverse pat
+                    log('execcomplete2');
                     stat.status = -1;
+                }else{ // regular pat
+                    log('execcomplete1');
+                    o[hash].status = 1;
+                    o[hash].childWindow.close();
                 }
-                window.removeEventListener('hashchange', execohc);
+                window.removeEventListener('execcomplete', execohc);
             }
         });
     });
 }
-function hashchange(hash){
-    var isEvent = null;
-    if(hash && typeof(hash) == 'object') {
-        isEvent = hash;
-        hash = location.hash;// hash was Event
+
+function hashchange(event, hash){
+    log('exec:hashchange:', hash);
+    if(event) {
+        hash = location.hash; // hash was Event
     }
-    if(!sessionStorage.hashstart) setTimeout(urlcleanup.bind(window, isEvent));
+    if(!sessionStorage.hashstart) setTimeout(urlcleanup.bind(window, event));
     window.onexec(hash);
-    window.dispatchEvent(new CustomEvent('exec'));
+    var e = new CustomEvent('exec');
+    e.data = hash;
+    window.dispatchEvent(e);
 }
+
 var selectByQuery = function(calc){
     if(!calc || !calc.match('^[a-zA-Z_][a-zA-Z0-9\\-_]*$')) return false;
     return document.querySelector(
-        '#' + calc + ' :where(textarea,input[type=text],input[type=number],input:not([type=number])), [name=' + calc + ']'
+        '#' + calc + ' :where(textarea,input:not([type]),input[type=text],input[type=number]), [name=' + calc + ']'
     );
 }
+
 window.onexec = function(hash){
     var el = null, calc = null, v = null, t = null, er = null, mt = null;
     var orighash = hash;
     hash = decodeURIComponent(hash);
-    // console.log('onexec', hash, hash.match(new RegExp('^#([^\\+]*)(?:\\+([\\s\\S]+)$)?', 'igm')), ';', RegExp.$1, ';', RegExp.$2); //document.querySelector('exec-result') &&
+    log('onexec: strt:', hash, ';', orighash);
     if(hash.match(new RegExp('^#([^\\+]*)(?:\\+([\\s\\S]+)$)?', 'igm')) &&
        (((((v = RegExp.$2) || 1) && (calc = RegExp.$1)) && (el = selectByQuery(calc))) || (
            !calc && v
        ))){
-        // console.log('onexec, fnd', calc, ';', v, mt);
         if(calc && (er = document.querySelector('exec-result, .exec-result'))){
             if(el) {
-                el.focus(); // focusing console.log('focusing', el);
+                el.focus(); 
             }
             var observer = null;
             // "ael.value = v" triggers calculation and result being set to er
-            (observer = new MutationObserver(function(mlist, observer){
-                for(var mutation of mlist){
-                    if(mutation.type == 'childList'){
-                        if(window.opened && opened[orighash]){
-                            t = window.opened[orighash].window;
-                            opened[orighash].status = 1;
-                        }else if(window.opener && window.opener.opened && opener.opened[orighash]){
-                            t = window.opener;
-                            opener.opened[orighash].status = 1;
-                        }else{
-                            console.warn('Window not found', opened, opened[orighash], opened[hash]);
-                            return;
+            if(v){
+                (observer = new MutationObserver(function(mlist, observer){
+                    for(var mutation of mlist){
+                        if(mutation.type == 'childList' && mutation.addedNodes.length){
+                            var port = null;
+                            log('onexec: childList update', opener, parentPort, orighash,
+                                        mutation);
+                            if(window.opened && opened[orighash]){ // reverse pat
+                                log('onexec: reverse', opened[orighash], orighash);
+                                port = window.opened[orighash].port;
+                                opened[orighash].status = 1;
+                            }else if(window.opener && parentPort){ // regular pat
+                                log('onexec: changeparent');
+                                port = parentPort;
+                            }else{
+                                console.warn('Window not found',
+                                             window.opened && opened, orighash);
+                                return;
+                            }
+                            // t.location.hash = '#+' + encodeURIComponent(er.innerText);
+                            er.classList.add('copying');
+                            var tx = er.innerText;
+                            er.classList.remove('copying');
+                            var result = '#+' + encodeURIComponent(tx);
+                            log('onexec: result text to postMessage', result, ';', tx);
+                            port.postMessage(result);
+                            port.postMessage({hash: orighash, result: result, type: 'complete'});
+                            observer.disconnect();
                         }
-                        t.location.hash = '#+' + er.innerText;
-                        // console.log('onexec: observer', er.innerText);
-                        observer.disconnect();
                     }
-                }
-            })).observe(er, {
-                attributes: false, childList: true,
-                subtree: false, characterdata: false
-            });
+                })).observe(er, {
+                    attributes: false, childList: true,
+                    subtree: false, characterdata: false
+                });
+            }
         }
         if(el || (document.activeElement && document.activeElement.matches(
             ':where(textarea,input)'))){
             var ael = (el || document.activeElement);
-            // console.log('commit event', ael);
+            log('onexec: set value', el, document.activeElement, v, (new Error()).stack);
             ael.value = v;
             var nev = new CustomEvent('keydown', {bubbles: true});
-            nev.target = ael;
+            Object.defineProperty(nev, 'target', {writable: false, value: ael});
+            Object.defineProperty(nev, 'keyCode', {writable: false, value: 13});
+            Object.defineProperty(nev, 'ctrlKey', {writable: false, value: true});
+            Object.defineProperty(nev, 'shiftKey', {writable: false, value: true});
             ael.dispatchEvent(nev);
             ael.dispatchEvent(new CustomEvent('change', {bubbles: true}));
             ael.dispatchEvent(new CustomEvent('input', {bubbles: true}));
@@ -125,17 +191,64 @@ window.onexec = function(hash){
     }
     return false;
 }
-window.addEventListener('hashchange', hashchange);
-if((location.hash && (location.hash.substr(0, 1) == '#')) || sessionStorage.hashstart){
-    if(urlcleanup()){
-        if(sessionStorage.hashstart){
-            window.hashstart = sessionStorage.hashstart;
-            // hashchange(sessionStorage.hashstart);
-            window.addEventListener('load', function(){hashchange(window.hashstart);});
-            sessionStorage.removeItem('hashstart');
-        }else{
-            var h = location.hash;
-            window.addEventListener('load', function(){hashchange(h);});
+
+if(!window.userw){
+    window.addEventListener('hashchange', hashchange);
+    
+    if((location.hash && (location.hash.substr(0, 1) == '#')) || sessionStorage.hashstart){
+        if(urlcleanup()){
+            if(sessionStorage.hashstart){
+                window.hashstart = sessionStorage.hashstart;
+                window.addEventListener('load', function(){
+                    hashchange(null, window.hashstart);
+                });
+                sessionStorage.removeItem('hashstart');
+            }else{
+                var h = location.hash;
+                window.addEventListener('load', function(){hashchange(null, h);});
+            }
         }
     }
 }
+
+var parentPort = null;
+function execonload(ev){
+    log('exec: load: opener', window.opener, window);
+    
+    if(window.opener){
+        var ch = new MessageChannel();
+        log('exec: load: set channel', ch);
+        var pp = ch.port1;
+        pp.window = opener;
+        pp.onmessage = function(e){
+            log('exec: load: onmessage', e, e.data);
+            if(!parentPort){
+                log('exec: load: loaded, sent port2 and succeeded');
+                parentPort = pp;
+                parentPort.href = e.data;
+                window.parentPort = pp;
+            }else if(typeof(e.data) == 'string'){
+                log('exec: load: changing href', e.data);
+                changeLocationHash(e.data);
+            }else if(e.data.type == 'complete'){
+                var ev = new CustomEvent('execcomplete');
+                ev.hash = e.data.hash;
+                ev.result = e.data.result;
+                window.dispatchEvent(ev);
+            }
+        }
+        opener.postMessage('init', '*', [ch.port2]);
+    }
+}
+if(window == top){
+    if(document.readyState == 'complete'){
+        execonload();
+    }else{
+        window.addEventListener('load', execonload);
+    }
+}else{
+    console.warn('non top window');
+}
+
+window.exec = exec;
+
